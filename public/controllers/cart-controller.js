6baea8fa33f0a283fd0ac8d392bc933a55f57c23
Aaron Roberson <aaronaroberson@gmail.com>
2014-06-09 22:09:47 -0700

    Day 8 completed work

--- public/controllers/cart-controller.js
+++ public/controllers/cart-controller.js
@@ -2,30 +2,21 @@
 
     var app = angular.module('MyStore');
 
-	// Inject in the ProductService
-    app.controller('CartController', function($scope) {
+	// Inject in the CartService
+    app.controller('CartController', function($scope, CartService) {
 
-	    // Set the items on the scope to the items in the ProductService
-	    $scope.items;
+	    // Set the items on the scope to the items in the CartService
+	    $scope.items = CartService.getItems();
 
-	    $scope.addItem = function(item) {
-		    // Add the item using the ProductService
+        $scope.getItemCount = CartService.getItemCount;
 
-	    };
+	    $scope.removeItem = CartService.removeItem;
 
-	    $scope.removeItem = function(item) {
-		    // Remove the item using the ProductService
+        $scope.emptyCart = CartService.emptyCart;
 
-	    };
+	    $scope.cartSubtotal = CartService.getCartSubtotal;
 
-	    $scope.cartSubtotal = function() {
-		    // Returns the cartSubtotal from the ProductService
-
-	    };
-
-	    $scope.cartTotal = function() {
-		    // Returns the cartTotal from the ProductService
-	    }
+	    $scope.cartTotal = CartService.getCartTotal;
 
     });
 
--- public/services/cart-service.js
+++ public/services/cart-service.js
@@ -3,54 +3,70 @@
 
 	var app = angular.module('MyStore');
 
-	app.factory('CartService', function($http) {
+	app.factory('CartService', function() {
 
 		// Private items variable
-		var items = [];
+		var items = {};
 
 		// Angular factories return service objects
 		return {
 
 			getItems: function() {
-				// Returns items array
-
+				// Returns items object
+                return items;
 			},
 
 			addItem: function(item) {
 				// Checks if item already exists
 				// If it exists, updates the quantity
 				// If it doesn't exist, adds quantity property with value of 1 then
-				// pushes the item onto the items array
-
-			},
-
-			updateItem: function(item) {
-				// Updates the quantity of an item
-
+				// adds the item onto the items object
+                if(!items[item.guid]) {
+                    item.quantity = 1;
+                    items[item.guid] = item;
+                }else {
+                    items[item.guid].quantity += 1;
+                }
 			},
 
 			removeItem: function(item_id) {
 				// Removes an item from the items array
 				// Can use angular.forEach(array, function(item, index) to splice
-
+                delete items[item_id];
 			},
 
 			emptyCart: function() {
 				// Sets items array to empty array
-
+                items = {};
 			},
 
 			getItemCount: function() {
 				// returns number of items, including item quantity
-
+                var total = 0;
+                angular.forEach(items, function(item) {
+                    total += parseInt(item.quantity);
+                });
+                return total;
 			},
 
 			getCartSubtotal: function() {
 				// Return the item quantity times item price for each item in the array
+                var total = 0;
+                angular.forEach(items, function(item) {
+                   total += parseInt(item.quantity) * parseInt(item.isSpecial ? item.specialPrice : item.price);
+                });
+                return total;
 			},
 
 			getCartTotal: function() {
 				// Return the cartSubtotal plus shipping and handling
+                // Return the item quantity times item price for each item in the array
+                var total = 0;
+                angular.forEach(items, function(item) {
+                    // TODO add shipping
+                    total += parseInt(item.quantity) * parseInt(item.isSpecial ? item.specialPrice : item.price);
+                });
+                return total;
 			}
 		};
 
